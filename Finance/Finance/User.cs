96ed11cc1f5fd42a67c564dde2d0b6b8b658a7f9
MikolajMichalski <miki.michalski@gmail.com>
2017-05-17 22:03:34 +0200

    Created project and xaml elements
    Added some pages with buttons to navigation

---
+++ Finance/Finance/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Finance
+{
+    class User
+    {
+        public int UserId { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        
+
+        public User(int UserId, string Name, string LastName)
+        {
+            this.UserId = UserId;
+            this.Name = Name;
+            this.LastName = LastName;
+
+        }
+
+    }
+}
